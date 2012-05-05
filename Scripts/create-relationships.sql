1932bde72e025752312c07c897c2d101822f9cb9
William Wenzel <wrw3355@rit.edu>
2012-05-05 18:03:33 -0400

    Added script to create relationship tables.

---
+++ Scripts/create-relationships.sql
@@ -0,0 +1,87 @@
+/* Relationships */
+CREATE TABLE "company_quoted" (
+    id int NOT NULL,
+    company_id int NOT NULL,
+    quote_id int NOT NULL,
+    ship_to_address varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (company_id) REFERENCES Company(id),
+    FOREIGN KEY (quote_id) REFERENCES Quote(id)
+)
+
+CREATE TABLE "company_gets" (
+    id int NOT NULL,
+    company_id int NOT NULL,
+    lead_id int NOT NULL,
+    create_date date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (company_id) REFERENCES Company(id),
+    FOREIGN KEY (lead_id) REFERENCES Lead(id)
+)
+
+CREATE TABLE "lead_becomes" (
+    id int NOT NULL,
+    lead_id int NOT NULL,
+    opportunity_id int NOT NULL,
+    create_date date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (lead_id) REFERENCES Lead(id),
+    FOREIGN KEY (opportunity_id) REFERENCES Opportunity(id)
+)
+
+CREATE TABLE "opportunity_requests" (
+    id int NOT NULL,
+    opportunity_id int NOT NULL,
+    quote_id int NOT NULL,
+    create_date date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (opportunity_id) REFERENCES Opportunity(id),
+    FOREIGN KEY (quote_id) REFERENCES Quote(id)
+)
+
+CREATE TABLE "quote_consists_of" (
+    id int NOT NULL,
+    quote_id int NOT NULL,
+    product_id int NOT NULL,
+    quantity int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (quote_id) REFERENCES Quote(id),
+    FOREIGN KEY (product_id) REFERENCES Product(id)
+)
+
+CREATE TABLE "quote_becomes" (
+    id int NOT NULL,
+    quote_id int NOT NULL,
+    order_id int NOT NULL,
+    create_date date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (quote_id) REFERENCES Quote(id),
+    FOREIGN KEY (order_id) REFERENCES Order(id)
+)
+
+CREATE TABLE "quote_priced_with" (
+    id int NOT NULL,
+    quote_id int NOT NULL,
+    currency_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (quote_id) REFERENCES Quote(id),
+    FOREIGN KEY (currency_id) REFERENCES Currency(id)
+)
+
+CREATE TABLE "order_priced_with" (
+    id int NOT NULL,
+    order_id int NOT NULL,
+    currency_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id) REFERENCES Order(id),
+    FOREIGN KEY (currency_id) REFERENCES Currency(id)
+)
+
+CREATE TABLE "currency_converts_to" (
+    id int NOT NULL,
+    from_currency_id int NOT NULL,
+    to_currency_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (from_currency_id) REFERENCES Currency(id),
+    FOREIGN KEY (to_currency_id) REFERENCES Currency(id)
+)
