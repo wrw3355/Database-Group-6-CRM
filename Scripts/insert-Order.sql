3d4c678893dcfe40293f38c0478ae4ffacc0cdba
mam8270 <captainmccoy@gmail.com>
2012-05-11 15:06:37 -0400

    added files

---
+++ Scripts/insert-Order.sql
@@ -0,0 +1,10 @@
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','48535  Armenia Ave.','ship type 1','2012-05-09','77.80');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','70821  Malta Ln.','ship type 1','2012-05-04','145.56');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','6088 North Saint Helena Blvd.','ship type 1','2012-05-07','31.42');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','47012 South Colombia Ave.','ship type 1','2012-05-08','195.55');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','59442 West Canada Ct.','ship type 2','2012-05-08','144.94');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','5872 West Pitcairn Way','ship type 2','2012-05-10','63.46');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','63581 West Dominican Republic St.','ship type 3','2012-05-10','181.33');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','37655 North Mozambique Way','Etiam','2012-05-06','16.78');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','82681 West Evansville Blvd.','ship type 3','2012-05-10','195.27');
+INSERT INTO Order (description,ship_from_address,ship_type,close_type,total_price) VALUES ('Order Description','43071  Mequon Blvd.','ship type 1','2012-05-07','155.14');
